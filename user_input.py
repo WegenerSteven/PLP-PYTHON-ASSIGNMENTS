3bbddaa761d834176790886ff8c9c6cc42d7b733
Wegernersteven <wegenersteven580@gmail.com>
2024-03-13 07:45:57 +0300

    The commit changes the code to ask the user for their name, age, and location, and then prints out a personalized message. It also adds a dictionary with two keys, 1 and 2, and prints the third element of the list associated with key 1.

--- user_input.py
+++ user_input.py
@@ -1,6 +1,10 @@
  #Write a program that asks the user for their name, age, and location and then prints out a personalized message.
-name=input("What is your name? ")
+"""name=input("What is your name? ")
 age=input("How old are you? ")
 location=input("Where do you live? ")
 
-print("Your name is "+name+" you are "+age+" years old, you come from "+location)
+print("Your name is "+name+" you are "+age+" years old, you come from "+location)"""
+x={}
+x[2]=10
+x[1]=[20,30,40]
+print(x[1][2])
