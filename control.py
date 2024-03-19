e163dcd74aa754260cb63260c36171bc7e765646
Wegernersteven <wegenersteven580@gmail.com>
2024-03-19 12:49:28 +0300

    python added assignments and trials

--- control.py
+++
@@ -1,9 +0,0 @@
-
-
-marks = 80
-if marks < 30:
-    result = "Failed"
-elif marks > 75:
-    result = "Passed with distinction"
-else:
-    result = "Passed"
---
+++ introduction/control.py
@@ -0,0 +1,17 @@
+
+
+"""marks = 80
+if marks < 30:
+    result = "Failed"
+elif marks > 75:
+    result = "Passed with distinction"
+else:
+    result = "Passed"""
+pi=3.14159
+radius = 8.0
+height = 16
+baseArea = pi * radius ** 2
+  
+cylinderVolume = baseArea * height
+print(baseArea)
+print(cylinderVolume)
